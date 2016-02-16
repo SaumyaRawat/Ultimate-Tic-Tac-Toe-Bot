ef6133bfb46172dcf64afca4ce537c653f881cb8
Anjali Shenoy <anjali.shenoy@students.iiit.ac.in>
2016-02-16 17:44:59 +0530

    Delete files

--- ManualPlayer.py
+++
@@ -1,8 +0,0 @@
-class ManualPlayer:
-	def __init__(self):
-		pass
-	def move(self, temp_board, temp_block, old_move, flag):
-		print 'Enter your move: <format:row column> (you\'re playing with', flag + ")"	
-		mvp = raw_input()
-		mvp = mvp.split()
-		return (int(mvp[0]), int(mvp[1]))
