559807a7a4a0f00155b56987627aee3004c61e5c
Anjali Shenoy <anjali.shenoy@students.iiit.ac.in>
2016-02-16 18:56:29 +0530

    Player 13

---
+++ Player13.py
@@ -0,0 +1,18 @@
+#python
+from evaluator_code import *
+
+class Player13:
+	def __init__(self):
+		pass
+
+	def move(self, board, block, old_move, flag):
+		#List of permitted blocks, based on old move.
+		blocks_allowed  = determine_blocks_allowed(old_move, temp_block)
+		#Get list of empty valid cells
+		cells = get_empty_out_of(temp_board, blocks_allowed)
+		#Choose a move based on some algorithm, here it is a random move.
+		print blocks_allowed
+		print cells
+
+obj = Player13()
+
