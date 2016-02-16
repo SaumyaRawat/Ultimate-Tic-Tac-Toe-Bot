c99daf6a0b6c309f04f72ce8fcf9664a4f54b241
Anjali Shenoy <anjali.shenoy@students.iiit.ac.in>
2016-02-16 14:21:34 +0530

    Modularised the files for convenience

---
+++ ManualPlayer.py
@@ -0,0 +1,8 @@
+class ManualPlayer:
+	def __init__(self):
+		pass
+	def move(self, temp_board, temp_block, old_move, flag):
+		print 'Enter your move: <format:row column> (you\'re playing with', flag + ")"	
+		mvp = raw_input()
+		mvp = mvp.split()
+		return (int(mvp[0]), int(mvp[1]))
